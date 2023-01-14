35cbaaac29f4ae14a7e99285fe26070cff5adedd
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2023-01-13 18:30:34 -0600

    Avances en la creación del proyecto y en primeros cambios

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <body>
+    <RouterView />
+  </body>
+</template>
+
+<style scoped>
+body {
+  max-height: 100vh;
+  min-width: 100vw;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Table from "../components/Table.vue";
+</script>
+
+<template>
+  <div class="flex flex-row">
+    <div class="basis-1/2">
+      <Table />
+    </div>
+  </div>
+</template>
